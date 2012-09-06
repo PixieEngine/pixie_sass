9c4f7df796b02fde788f8e484d2b079f60e957b8
Daniel X. Moore <yahivin@gmail.com>
2012-09-06 11:35:01 -0700

    Moved stylesheets into lib directory and added compass framework registration

--- lib/pixie_sass.rb
+++ lib/pixie_sass.rb
@@ -1,5 +1,10 @@
 require "pixie_sass/version"
 
+require "compass"
+
+base_directory = File.join(File.dirname(__FILE__))
+Compass::Frameworks.register('pixie_sass', :path => base_directory)
+
 module PixieSass
   # Your code goes here...
 end
